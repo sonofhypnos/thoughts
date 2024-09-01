ab715d5249c8903ddd8a4fe7e9b653fc5488c6e3
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-09-01 23:45:51 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./convert_notebooks.sh
+#./convert_notebooks.sh # NOTE: not converting by default, because it's slow
 
 /home/tassilo/.cache/pypoetry/virtualenvs/thoughts-nVi2q3I2-py3.10/bin/python generate_blog.py
 
